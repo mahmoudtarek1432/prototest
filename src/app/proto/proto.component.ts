bf550d7eea202f28e33dd26bca9d0e9a710422d7
ShadyNagyE <shady.elsayed@ecgsa.com>
2023-03-16 15:02:19 +0200

    init

--- src/app/proto/proto.component.ts
+++ src/app/proto/proto.component.ts
@@ -4,6 +4,8 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { Message } from "protobufjs"
 import { ProtobufType, ProtoWrapper } from 'src/ProtoWraper/protowrapper';
 import { timeInterval } from 'rxjs';
+import { ProtoHelper } from '../helper/proto-helper';
+import { Awesome } from '../models/awesome';
 
 @Component({
   selector: 'app-proto',
@@ -18,8 +20,22 @@ export class ProtoComponent {
   constructor(){
 
   }
-  encode(){
+  
+  async encode2(){
+    const awsomeData = new Awesome();
+    awsomeData.awesomeField = this.prototext;
+    let protoHelper = await ProtoHelper.encode('./assets/protos/webapi_3.proto', 'awesome_3', 'Awesome', awsomeData);
+    console.log(protoHelper);
+    console.log(ProtoHelper.toHexString(protoHelper));
+  }
 
+  async decode2(){
+    const bytes = ProtoHelper.fromHexString(this.prototext);
+    let protoHelper = await ProtoHelper.decode<Awesome>('./assets/protos/webapi_3.proto', 'awesome_3', 'Awesome', bytes);
+    console.log(protoHelper);
+  }
+
+  encode(){
     var payload = {"awesomeField": this.prototext}
     this.protoFileAccessor((message) =>{
       var errmsg = message?.verify(payload)
