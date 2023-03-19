3816d45714fb0e5a56fb5f9b11ac4d9c0c406dc2
mahmoudtarek1432 <43779204+mahmoudtarek1432@users.noreply.github.com>
2023-03-19 15:15:10 +0200

    --

--- src/app/EndpointHandlers/Response/ResponseService.ts
+++ src/app/EndpointHandlers/Response/ResponseService.ts
@@ -1,10 +1,15 @@
 import { AuthEndpoint } from "src/app/Endpoints/AuthEndpoint";
 import { loginEndpoint } from "src/app/Endpoints/LoginEndopoint";
 import { ResponseManager } from "src/app/models/EndpointResponseManager";
+import { LoginResponse } from "src/app/models/LoginRequest";
 import { ResponseEndpointHelper } from "./ResponseEndpointHelper";
 
 export class ResponseService{
-    responseHelper!: ResponseEndpointHelper
+    responseHelper: ResponseEndpointHelper = new ResponseEndpointHelper()
+    
+    constructor(){
+        this.responseHelper.AddResponseEndpoint(LoginResponse, new loginEndpoint().handleLogin)
+    }
     //function called by websocketHandler
 }
 
