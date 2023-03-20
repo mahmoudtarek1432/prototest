85192b6bbf5f5261ff50bb3c48ca3698db28d4a7
mahmoudtarek1432 <43779204+mahmoudtarek1432@users.noreply.github.com>
2023-03-20 16:27:16 +0200

    Update EndpointResponseFactory.ts

--- src/app/Endpoints/EndpointResponseFactory.ts
+++ src/app/Endpoints/EndpointResponseFactory.ts
@@ -7,10 +7,10 @@ import { IResponseEndpoint } from "./IResponseEndpoint";
  * according to the incoming response id, the services gets injected : if decorated with injectable.
  */
 export class EndpointResponseFactory{
-   /* static Create<T extends IResponseEndpoint>(id: number): T{
+     static Create<T extends IResponseEndpoint>(id: number): T{
         return AppModule.injectorInstance.get(this.endpoints[id],new Error("endpoint not found")) //depricated
     }
     private static endpoints:{[id:number] : any} ={
         1 : LoginService
-    }*/
+    }
 }
