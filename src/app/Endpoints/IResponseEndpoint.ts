926c593e79e92b51b48b4d7815511d9d864b4c55
mahmoudtarek1432 <43779204+mahmoudtarek1432@users.noreply.github.com>
2023-03-21 16:28:11 +0200

    Update IResponseEndpoint.ts

--- src/app/Endpoints/IResponseEndpoint.ts
+++ src/app/Endpoints/IResponseEndpoint.ts
@@ -1,9 +1,11 @@
 import { EndpointsSubjects } from "src/Shared/Endpoints-Subjects";
+import { EndpointsMap } from "src/Shared/EnpointMap";
 import { SubjectHandler } from "../helper/Subject-helper";
 
-export abstract class IResponseEndpoint{
+export abstract class IResponseEndpoint<Response,endpoint>{
     constructor(subject: EndpointsSubjects){
-        
+        let x =
+        EndpointsMap.CreateEndpoint(Response,endpoint)
     }
     abstract handle(handleData:any):void;
 }
