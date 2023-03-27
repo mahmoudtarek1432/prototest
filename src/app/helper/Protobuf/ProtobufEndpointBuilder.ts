8cc7235d0a40b27e21e4a81c38ea70ad5485cd26
mahmoudtarek1432 <43779204+mahmoudtarek1432@users.noreply.github.com>
2023-03-27 10:52:30 +0200

    cleanup

--- src/app/helper/Protobuf/ProtobufEndpointBuilder.ts
+++ src/app/helper/Protobuf/ProtobufEndpointBuilder.ts
@@ -22,30 +22,15 @@ export class ProtobufEndpointBuilder{
                        package Endpoint;\
                        message RequestEndpoints{\ "
 
-                       //push messages from array
-                       this.protoMessageFiles.forEach((file,i) =>{
-                endpoint = endpoint + `repeated ${file.messageName} ${this.configureMessageName(file.messageName)} = ${i};\ `
-            })
-
-
+        //push messages from array
+        this.protoMessageFiles.forEach((file,i) =>{
+            let FieldName = ProtoFileStringManipulation.configureMessageName(file.messageName)+"s"
+            endpoint = endpoint + `repeated ${file.messageName} ${FieldName} = ${i+1};\ `
+        })
 
         endpoint = endpoint + "}";
                         
         return endpoint;        
-    }
-
-    //converts messageName into memberName by ignoring first letter and inserting an _ before capital letters asuming the name is in js nameing conventions
-    configureMessageName(messageName: string){
-        let newString = ''
-        for(let i = 1; i < messageName.length; i++){
-            if(65 <= messageName[i].charCodeAt(0) &&  messageName[i].charCodeAt(0) <= 90){ // 65 is for A and 90 is for Z
-                let righthand = messageName.slice(0,i);
-                let lefthand = messageName.slice(i,messageName.length);
-                newString = righthand + "_" + lefthand
-
-            }
-        }
-        return newString.toLowerCase()
     }
 }
 
